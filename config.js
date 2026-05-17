78280f611f3a86b359b4c077a3e5825ea06f821c
syestem <xanaxoriginal@gmail.com>
2026-05-18 04:09:48 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
-window.MiniAppConfig = {
-  apiBaseUrl: "http://localhost:8000",
-  appBackend: "local-api",
-};
+(function () {
+  const params = new URLSearchParams(window.location.search);
+  const hash = window.location.hash.startsWith("#") ? window.location.hash.slice(1) : window.location.hash;
+  const hashParams = new URLSearchParams(hash);
+  const apiBaseUrl = params.get("apiBaseUrl") || hashParams.get("apiBaseUrl") || "http://localhost:8000";
+
+  window.MiniAppConfig = {
+    apiBaseUrl: apiBaseUrl.replace(/\/$/, ""),
+    appBackend: "local-api",
+  };
+})();
