981a6e46d0b176130d0eefbe11fb8299c2b4d38c
syestem <xanaxoriginal@gmail.com>
2026-05-18 04:22:10 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   const params = new URLSearchParams(window.location.search);
   const hash = window.location.hash.startsWith("#") ? window.location.hash.slice(1) : window.location.hash;
   const hashParams = new URLSearchParams(hash);
-  const apiBaseUrl = params.get("apiBaseUrl") || hashParams.get("apiBaseUrl") || "http://localhost:8000";
+  const apiBaseUrl = params.get("apiBaseUrl") || hashParams.get("apiBaseUrl") || "https://billy-lactiferous-javier.ngrok-free.dev";
 
   window.MiniAppConfig = {
     apiBaseUrl: apiBaseUrl.replace(/\/$/, ""),
