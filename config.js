b9a9c8c06e84a542dc6d42ab13237d757dcd3a51
syestem <xanaxoriginal@gmail.com>
2026-05-18 09:47:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   const params = new URLSearchParams(window.location.search);
   const hash = window.location.hash.startsWith("#") ? window.location.hash.slice(1) : window.location.hash;
   const hashParams = new URLSearchParams(hash);
-  const apiBaseUrl = params.get("apiBaseUrl") || hashParams.get("apiBaseUrl") || "__MINI_APP_API_URL__";
+  const apiBaseUrl = params.get("apiBaseUrl") || hashParams.get("apiBaseUrl") || "https://max-abonements-api-syestem.amvera.io";
 
   window.MiniAppConfig = {
     apiBaseUrl: apiBaseUrl.replace(/\/$/, ""),
